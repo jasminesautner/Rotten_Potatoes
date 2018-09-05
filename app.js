a840ac4f7937b097e697dd0e7dd2233db06eaa58
jasminesautner <jasmineivysautner@gmail.com>
2018-09-05 10:32:09 -0700

    Users can see all reviews

--- app.js
+++ app.js
@@ -2,15 +2,19 @@
 const express = require('express')
 const app = express()
 var exphbs = require('express-handlebars');
-
+// OUR MOCK ARRAY OF PROJECTS
+let reviews = [
+    { title: "Great Review"},
+    { title: "Next Review"}
+]
+// INDEX 
 app.get('/', (req, res) => {
-    res.render('home', { msg: 'Hello World!'});
-})
-
-app.listen(3000, () => {
-    console.log('App listening on port 3000!')
+    res.render('reviews-index', { reviews: reviews });
 })
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
+app.listen(3000, () => {
+    console.log('App listening on port 3000!')
+})

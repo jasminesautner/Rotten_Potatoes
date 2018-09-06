d77ed4e995e18249f408f477abefeab06f56281d
jasminesautner <jasmineivysautner@gmail.com>
2018-09-05 21:01:59 -0700

    users can create reviews

--- app.js
+++ app.js
@@ -6,15 +6,21 @@ app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/rotten_potatoes', { useNewUrlParser: true });
+// INITIALIZE BODY-PARSER AND ADD IT TO APP
+const bodyParser = require('body-parser');
 const Review = mongoose.model('Review', {
-    title: String
+    title: String,
+    description: String,
+    movieTitle: String
 });
+// The following line must appear AFTER const app = expres() and before your routes!
+app.use(bodyParser.urlencoded({ extended: true}));
 
-// // OUR MOCK ARRAY OF PROJECTS
-// var reviews = [
-//     { title: this.title},
-//     { title: this.moveTitle}
-// ]
+// OUR MOCK ARRAY OF PROJECTS
+var reviews = [
+    { title: this.title},
+    { title: this.moveTitle}
+]
 
 // INDEX 
 app.get('/', (req, res) => {
@@ -25,6 +31,23 @@ app.get('/', (req, res) => {
         console.log(err);
     })
 })
+
+// NEW
+app.get('/reviews/new', (req, res) => {
+    console.log(req.body);
+    // res.render('reviews-new', {});
+})
+
+// CREATE
+app.post('/reviews', (req, res) => {
+    Review.create(req.body).then((review) => {
+        console.log(review);
+        res.redirect('/');
+    }).catch((err) => {
+        console.log(err.message);
+    })
+})
+
 app.listen(3000, () => {
     console.log('App listening on port 3000!')
 })

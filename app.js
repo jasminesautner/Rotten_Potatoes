000d7ffa95e4d8b7fb7dfbd1ec090a775483f4c4
jasminesautner <jasmineivysautner@gmail.com>
2018-09-05 21:30:35 -0700

    User can see single reviews

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ app.get('/', (req, res) => {
 })
 
 // NEW
-app.get('/reviews/new', (req, res) => {
+app.get('/reviews-new', (req, res) => {
     console.log(req.body);
     // res.render('reviews-new', {});
 })
@@ -42,7 +42,16 @@ app.get('/reviews/new', (req, res) => {
 app.post('/reviews', (req, res) => {
     Review.create(req.body).then((review) => {
         console.log(review);
-        res.redirect('/');
+        res.redirect('/reviews/${review._id}') // Redirect to reviews/:id
+    }).catch((err) => {
+        console.log(err.message)
+    })
+})
+
+// SHOW
+app.get('/reviews/:id', (req, res) => {
+    Review.findById(req.params.id).then((review) => {
+        res.render('reviews-show', { review: review})
     }).catch((err) => {
         console.log(err.message);
     })

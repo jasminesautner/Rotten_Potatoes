6012b6011d3488292972b2c144d115f0331e7655
jasminesautner <jasmineivysautner@gmail.com>
2018-09-05 19:23:34 -0700

    moved app.engine/.set

--- app.js
+++ app.js
@@ -2,11 +2,13 @@
 const express = require('express')
 const app = express()
 var exphbs = require('express-handlebars');
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/rotten_potatoes', { useNewUrlParser: true });
 const Review = mongoose.model('Review', {
     title: String
-})
+});
 
 // // OUR MOCK ARRAY OF PROJECTS
 // var reviews = [
@@ -23,10 +25,6 @@ app.get('/', (req, res) => {
         console.log(err);
     })
 })
-
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
-app.set('view engine', 'handlebars');
-
 app.listen(3000, () => {
     console.log('App listening on port 3000!')
 })
